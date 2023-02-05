6210cca5192187a4f034277e67fd1d80eda84781
Krzysztof Perwel <kperwel@evolution.com>
2023-02-05 11:38:50 +0100

    Add manifest link

--- pages/index.tsx
+++ pages/index.tsx
@@ -68,6 +68,7 @@ export default function Home() {
         <meta name="description" content="The Thest" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.webmanifest" />
       </Head>
 
       <DndContext onDragStart={handleDragStart} onDragEnd={handleDragEnd}>
